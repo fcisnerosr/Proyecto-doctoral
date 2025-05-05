05379c05e4bbb87c0a13eb231ae7aeb6df1f5280
Laptop HP <fcisnerosr@outlook.es>
2025-05-04 23:35:59 -0600

    trabajo inicial de unaCorridaAG.m[F

--- code/config.m
+++ code/config.m
@@ -2,6 +2,6 @@
 function config = config()
   config.tipo        = "simple";
   config.rangoElem   = 1:120;
-  config.porcentajes = [5,10,15];
+  config.porcentajes = [5];
   config.outputFolder = fullfile(pwd, "resultados_AG");
 end
