364669b7c3bacba25345fd92ad23069bedd3e305
Laptop HP <fcisnerosr@outlook.es>
2025-05-13 17:45:18 -0600

    Ya se esta ejecutando el AG en varias corridas, falta guardar correctamente otros datos

--- code/config.m
+++ code/config.m
@@ -2,7 +2,9 @@
 function config = config()
   config.tipo        = "simple";
   config.tipo_dano   = 'corrosion'; 
-  config.rangoElem   = 1:120; 
-  config.porcentajes = 5:5:90;   % [5,10,15,…,90]
+  % config.rangoElem   = 1:120; 
+  % config.porcentajes = 5:5:90;   % [5,10,15,…,90]
+  config.rangoElem   = 1:2; 
+  config.porcentajes = 5:6;   % [5,10,15,…,90]
   config.outputFolder = fullfile(pwd, "resultados_AG");
 end
