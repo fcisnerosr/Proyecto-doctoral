589807784ddaefa03358752c73b71bc97032bc86
Francisco Cisneros <fcisnerosr@outlook.es>
2025-09-18 13:46:20 -0600

    quinto cambio, en config.m para la ejecucion de la abolladura

--- code/001_framework_resultados_journal/config.m
+++ code/001_framework_resultados_journal/config.m
@@ -1,12 +1,13 @@
 % config.m (función)
 function config = config()
   config.tipo        = "simple";
-  config.tipo_dano   = 'corrosion'; 
+  % config.tipo_dano   = 'corrosion'; 
+  config.tipo_dano   = 'abolladura'; 
   config.ab = struct('Nseg', 1000, 'Slong', 5, 'lim', 3e-3);
   % config.rangoElem   = 5:6; 
   % config.porcentajes = 5:5:10;   % [5,10,15,…,90]
   % config.rangoElem   = 1:120; 
   config.rangoElem   = 1; 
-  config.porcentajes = 5;   % [5,10,15,…,90]
+  config.porcentajes = 35;   % [5,10,15,…,90]
   config.outputFolder = fullfile(pwd, "resultados_AG");
 end
