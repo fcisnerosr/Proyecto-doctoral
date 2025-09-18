21959f88965d2345aa909b3faee2c51699022b6f
Francisco Cisneros <fcisnerosr@outlook.es>
2025-09-18 11:27:01 -0600

    feat: primer cambio en switch_case para integrar la abolladura

--- code/001_framework_resultados_journal/004_runExperimentos/switch_case_danos.m
+++ code/001_framework_resultados_journal/004_runExperimentos/switch_case_danos.m
@@ -20,11 +20,31 @@ function [ke_d_total, ke_d, prop_geom_mat] = switch_case_danos(no_elemento_a_dan
         switch lower(tipo)
             case 'corrosion'
                 ke_d(:,:,j) = ke_corrosion_uniform_from_prop( ...
-                    idxElemGlobal, L_d(j), dano_porcentaje(j), prop_geom_mat, ...
+                    idxElemGlobal, L_d(j), dano_porcentaje(j), procs ftp_geom_mat, ...
                     E(j), G(j), DIAM_COL, THICK_COL);
 
-            case 'abolladura'
-                error('Abolladura no implementada aquí (usa tu pipeline de abolladura).');
+                case 'abolladura'
+                % --- indices y entradas ---
+                j    = idxElem;                % ya lo tienes en tu for
+                idx  = idxElemGlobal;          % no_elemento_a_danar(j)
+                L    = L_d(j);
+                D    = prop_geom_mat(idx, 11); % diámetro exterior (col 11)
+                t    = prop_geom_mat(idx, 12); % espesor (col 12)
+                Eel  = E(j);
+                Gel  = G(j);
+                nu   = Eel/(2*Gel) - 1;        % Poisson por consistencia: G=E/2(1+nu)
+
+                porcent = dano_porcentaje(j);
+                assert(porcent < 50, 'La abolladura debe ser < 50%% del diámetro.');
+
+                % --- controles numéricos (defaults o desde config, ver abajo) ---
+                Nseg  = ab_opts.Nseg;
+                Slong = ab_opts.Slong;
+                lim   = ab_opts.lim;
+
+                % --- construir Ke ---
+                [ke_loc, ~, ~] = ab_build_element_ke(porcent, D, t, L, Eel, nu, Nseg, Slong, lim);
+                ke_d(:,:,j) = ke_loc;
 
             otherwise
                 error('Caso de daño "%s" no soportado.', tipo);
