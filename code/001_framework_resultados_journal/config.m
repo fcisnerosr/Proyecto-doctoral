861cdd7bacc22f945c5e71376745996a43bcfc50
Francisco Cisneros <fcisnerosr@outlook.es>
2025-09-30 15:11:13 -0600

    test: config para la deteccion de abolladuras en todos los elementos de la subestructura

--- code/001_framework_resultados_journal/config.m
+++ code/001_framework_resultados_journal/config.m
@@ -4,10 +4,7 @@ function config = config()
   % config.tipo_dano   = 'corrosion'; 
   config.tipo_dano   = 'abolladura'; 
   config.ab          = struct('Nseg', 1000, 'Slong', 5, 'lim', 3e-3);
-  % config.rangoElem   = 5:6; 
-  % config.porcentajes = 5:5:10;   % [5,10,15,…,90]
-  % config.rangoElem   = 1:120; 
-  config.rangoElem   = 71; 
-  config.porcentajes = 49;   % [5,10,15,…,90]
+  config.porcentajes = 5:5:50;   % [5,10,15,…,90]
+  config.rangoElem   = 1:120;
   config.outputFolder = fullfile(pwd, "resultados_AG");
 end
