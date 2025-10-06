0e73c004f3314be61ddbd3a567af6fe3d85a8d05
Francisco Javier Cisneros Ruiz <fcisnerosr@outlook.es>
2025-10-06 04:45:00 -0600

    test: resultados abolladuras

--- code/001_framework_resultados_journal/config.m
+++ code/001_framework_resultados_journal/config.m
@@ -4,7 +4,7 @@ function config = config()
   % config.tipo_dano   = 'corrosion'; 
   config.tipo_dano   = 'abolladura'; 
   config.ab          = struct('Nseg', 1000, 'Slong', 5, 'lim', 3e-3);
-  config.porcentajes = 5:5:50;   % [5,10,15,…,90]
+  config.porcentajes = 5:5:45;   % [5,10,15,…,90]
   config.rangoElem   = 1:120;
   config.outputFolder = fullfile(pwd, "resultados_AG");
 end
